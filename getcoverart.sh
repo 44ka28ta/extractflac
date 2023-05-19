89317be686d85acd5730db13431502699c25285e
44ka28ta <44ka28ta@gmail.com>
2023-05-19 18:24:12 +0900

    Add the option to choice Discogs as an image source
    Add the option to choice Discogs as an image source in getcoverart.sh.

--- getcoverart.sh
+++ getcoverart.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-get_src_from_img_element_with_xpath() {
+get_src_from_specific_element_with_xpath() {
 	local ELEMENTSTR=$(xmllint --html --shell $1 2> /dev/null <<EOF
-cat //img[@id="$2"]/@src
+cat //$2/@$3
 bye
 EOF
 )
 
-	local ELEMENTSTR="`echo ${ELEMENTSTR} | grep -a -E 'src' | sed -E 's/^.*src="(.*)".*$/\1/'`"
+	local ELEMENTSTR="`echo ${ELEMENTSTR} | grep -a -E $3 | sed -E 's/^.*'$3'="(.*)".*$/\1/'`"
 
-	# Fifth argument is defined return value.
-	local -n ELEMENTS=$3
+	# Third argument is defined return value.
+	local -n ELEMENTS=$4
 
 	while IFS= read -r LINE; do
 
@@ -26,18 +26,20 @@ modify_amazon_image_url() {
 }
 
 show_help() {
-        echo "Get the cover art image file from Amazon.co.jp" >&2
-        echo "$0 [-h] [-f FILENAME] -c AMAZON_CODE" >&2
+        echo "Get the cover art image file from Amazon.co.jp or Discorgs" >&2
+        echo "$0 [-h] [-d] [-f FILENAME] -c CODE" >&2
         echo "" >&2
-	echo "-c: specify the commodity code. AMAZON_CODE is in https://www.amazon.co.jp/dp/AMAZON_CODE ." >&2
+	echo "-c: specify the commodity code. CODE is in the case of https://www.amazon.co.jp/dp/CODE or https://www.discogs.com/ja/release/CODE-XXX." >&2
 	echo "-f: specify the name of downloaded cover art file." >&2
+	echo "-d: specify Discogs as the image source." >&2
         echo "-h: show this." >&2
 }
 
 FILE_NAME='folder.jpg'
-AMAZON_CODE=''
+CODE=''
+ALTIMAGESRC=''
 
-while getopts "h?f:c:" opt; do
+while getopts "h?f:c:d" opt; do
 	case "${opt}" in
 		h|\?)
 			show_help
@@ -47,27 +49,42 @@ while getopts "h?f:c:" opt; do
 			FILE_NAME=${OPTARG}
 			;;
 		c)
-			AMAZON_CODE=${OPTARG}
+			CODE=${OPTARG}
+			;;
+		d)
+			ALTIMAGESRC='https://www.discogs.com/ja/release/'
 			;;
 	esac
 done
 
-if [ -z "${AMAZON_CODE}" ]; then
+if [ -z "${CODE}" ]; then
         show_help
         exit 0
 fi
 
 
-COMMODITYXMLNAME=${AMAZON_CODE}.xml
+COMMODITYXMLNAME=${CODE}.xml
 USER_AGENT="Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0"
 ACCEPT_ENCODING="gzip,deflate"
 
-curl -L -X GET https://www.amazon.co.jp/dp/${AMAZON_CODE} -H "User-Agent: ${USER_AGENT}, Accept-Encoding:${ACCEPT_ENCODING}" | xmllint --html --format - 2> /dev/null > ${COMMODITYXMLNAME}
+if [ -z "${ALTIMAGESRC}" ]; then
+	AMAZON_SOURCE='https://www.amazon.co.jp/dp/'
 
-IMAGE_URL=()
+	curl -L -X GET ${AMAZON_SOURCE}${CODE} -H "User-Agent: ${USER_AGENT}, Accept-Encoding:${ACCEPT_ENCODING}" | xmllint --html --format - 2> /dev/null > ${COMMODITYXMLNAME}
 
-get_src_from_img_element_with_xpath ${COMMODITYXMLNAME} "landingImage" IMAGE_URL
+	IMAGE_URL=()
 
-curl -o ${FILE_NAME} $(modify_amazon_image_url ${IMAGE_URL[0]})
+	get_src_from_specific_element_with_xpath ${COMMODITYXMLNAME} 'img[@id="landingImage"]' 'src' IMAGE_URL
 
+	curl -o ${FILE_NAME} $(modify_amazon_image_url ${IMAGE_URL[0]})
+else
+	curl -L -X GET ${ALTIMAGESRC}${CODE} -H "User-Agent: ${USER_AGENT}, Accept-Encoding:${ACCEPT_ENCODING}" | xmllint --html --format - 2> /dev/null > ${COMMODITYXMLNAME}
 
+	IMAGE_URL=()
+
+	get_src_from_specific_element_with_xpath ${COMMODITYXMLNAME} 'meta[@property="og:image"]' 'content' IMAGE_URL
+
+	curl -o ${FILE_NAME} ${IMAGE_URL[0]}
+fi
+
+rm ${COMMODITYXMLNAME}
